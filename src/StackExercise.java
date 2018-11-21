38ba41941ed5aec945425218f511950232dbe932
Chan Lee <chanethanlee@gmail.com>
2018-11-21 15:15:09 -0800

    edit vps to return all test case simultaneously.

--- src/StackExercise.java
+++ src/StackExercise.java
@@ -1,48 +1,40 @@
 import java.util.Scanner;
-
 public class StackExercise {
-
     public static void main(String[] args) {
-//        Stack<Integer> stack = new Stack<>();
-//        stack.push(1);
-//        System.out.println(stack.pop());
-//        System.out.println(stack.peek());
-//        System.out.println(stack.size());
-        ValidParenthesisString();
-
+        ValidParethesisString();
     }
 
-    public static void ValidParenthesisString() {
+
+    public static void ValidParethesisString() {
         Scanner sc = new Scanner(System.in);
-        int cnt = 0;
+
         int testCase = sc.nextInt();
         for (int n = 0; n < testCase; n++) {
             String ps = sc.next();
-            for (int i = 0; i < ps.length(); i++) {
-                if (ps.charAt(i) == '(') {
-                    cnt++;
-                } else {
-                    cnt--;
-                    if (cnt < 0) {
-                        System.out.println("No");
-                    }
-                }
-            }
-
-            if (cnt == 0) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
+            System.out.println(valid(ps));
         }
-
         sc.close();
     }
 
-
-
-
+    public static String valid (String ps) {
+        int cnt = 0;
+        for (int i = 0; i < ps.length(); i++) {
+            if (ps.charAt(i) == '(') {
+                cnt++;
+            } else {
+                cnt--;
+                if (cnt < 0) {
+                    return "NO";
+                }
+            }
+        }
+        if (cnt == 0) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
 }
-//2
-//        ()
-//        ())
+
+
+
